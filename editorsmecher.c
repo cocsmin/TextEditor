43b52db2e2ef7372673e6367bd2b0eb15b5e02bf
Cosmin-Daniel <cosminsecrier@yahoo.com>
2024-12-26 13:56:40 +0200

    added a simple function to enable raw mode for the terminal

--- editorsmecher.c
+++ editorsmecher.c
@@ -1,3 +1,21 @@
+#include <unistd.h>
+#include <unistd.h>
+
+void enableRaw(){
+	struct termios raw;
+
+	tcgetattr(STDIN_FILENO, &raw);
+
+	raw.c_lflag &= ~(ECHO);
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	
+}
+
 int main(){
+	enableRaw();
+
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1);
 	return 0;
 }
