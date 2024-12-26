e92698e0c3c2dc3654a0147512ed294a7dcfcb32
Cosmin-Daniel <cosminsecrier@yahoo.com>
2024-12-26 14:52:42 +0200

    disabled some more flags and added some error handling

--- editorsmecher.c
+++ editorsmecher.c
@@ -3,23 +3,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <errno.h>
+
 
 struct termios orig_termios;
 
+void die(const char* s){
+	perror(s);
+	exit(1);
+}
+
 void disableRaw(){
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+		die("tcsetattr");
 }
 
 void enableRaw(){
-	tcgetattr(STDIN_FILENO, &orig_termios);
+	if(tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+		die("tcgetattr");
 
 	atexit(disableRaw);
 
 	struct termios raw = orig_termios;
 
-	raw.c_lflag &= ~(ECHO | ICANON);
+	raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
 
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
+
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+		die("tcsetattr");
 	
 }
 
@@ -27,13 +43,17 @@ int main(){
 	enableRaw();
 
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+	while (1){
+		char c = '\0';
+		if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+			die("read");
 		if (iscntrl(c)){
-			printf("%d\n", c);
+			printf("%d\r\n", c);
 		}
 		else {
-			printf("%d ('%c')\n", c, c);
+			printf("%d ('%c')\r\n", c, c);
 		}
+		if (c == 'q') break;
 	}
 	return 0;
 }
